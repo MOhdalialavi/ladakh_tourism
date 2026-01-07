a10682ace0ae360eae8cbd41ed376fd3892c76c7
MOhdalialavi <120246642+MOhdalialavi@users.noreply.github.com>
2026-01-07 20:05:31 +0530

    i changed some this in the app

--- ladakhtourism/settings.py
+++ ladakhtourism/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tourism'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'ladakhtourism.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'tourism/template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
